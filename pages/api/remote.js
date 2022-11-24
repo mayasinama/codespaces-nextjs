353bccbb2bb28ddf7b326337836712c367100b97
mayasinama <114096128+mayasinama@users.noreply.github.com>
2022-11-24 03:19:15 +0000

    a

--- pages/api/remote.js
+++ pages/api/remote.js
@@ -13,7 +13,7 @@ export default async function (req, res) {
         
         const browser = await puppeteer.launch({
           executablePath,
-          args: [...edgeChromium.args, '--user-agent=Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0'],
+          args: [...edgeChromium.args],
           headless: false,
         })
     
