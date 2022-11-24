180dd4311cd563e52eee3aac9481e89d4198745a
mayasinama <114096128+mayasinama@users.noreply.github.com>
2022-11-24 01:47:58 +0000

    update final

--- pages/api/remote.js
+++ pages/api/remote.js
@@ -8,26 +8,51 @@ import puppeteer from 'puppeteer-core'
 import cheerio from 'cheerio'
 
 export default async function (req, res) {
-    // Edge executable will return an empty string locally.
-    const executablePath = await edgeChromium.executablePath
+    if(req.method === 'GET') {
+        const executablePath = await edgeChromium.executablePath
+        
+        const browser = await puppeteer.launch({
+          executablePath,
+          args: edgeChromium.args,
+          headless: false,
+        })
     
-    const browser = await puppeteer.launch({
-      executablePath,
-      args: edgeChromium.args,
-      headless: false,
-    })
-
-    // await page.goto('https://animixplay.to/v1/do-it-yourself/ep8', { waitUntil: 'networkidle0' });
-
-    const page = await browser.newPage()
-    await page.goto('https://animixplay.to/v1/do-it-yourself/ep8')
-
-    const data = await page.content()
-
-    await browser.close();
-
-    const $ = cheerio.load(data)
-    const dummy = $.html()
+        // await page.goto('https://animixplay.to/v1/do-it-yourself/ep8', { waitUntil: 'networkidle0' });
     
-    res.json({dummy})
+        const page = await browser.newPage()
+        await page.goto('https://animixplay.to/v1/do-it-yourself/ep8', { waitUntil: 'networkidle0' })
+    
+        const data = await page.content()
+    
+        await browser.close();
+    
+        // const $ = cheerio.load(data)
+        // const dummy = $.html()
+        
+        res.json({data})
+    }
+    if(req.method === 'POST') {
+        const {url} = req.body
+        const executablePath = await edgeChromium.executablePath
+        
+        const browser = await puppeteer.launch({
+          executablePath,
+          args: edgeChromium.args,
+          headless: false,
+        })
+    
+        // await page.goto('https://animixplay.to/v1/do-it-yourself/ep8', { waitUntil: 'networkidle0' });
+    
+        const page = await browser.newPage()
+        await page.goto(url)
+    
+        const data = await page.content()
+    
+        await browser.close();
+    
+        const $ = cheerio.load(data)
+        const dummy = $.html()
+        
+        res.json({dummy})
+    }
   }
