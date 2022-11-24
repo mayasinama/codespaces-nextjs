1471dc5eaffd7528ff8053410e38aaf3bfe19d68
mayasinama <114096128+mayasinama@users.noreply.github.com>
2022-11-24 01:22:51 +0000

    update

---
+++ pages/api/remote.js
@@ -0,0 +1,27 @@
+// api/run.js
+import edgeChromium from 'chrome-aws-lambda'
+
+// Importing Puppeteer core as default otherwise
+// it won't function correctly with "launch()"
+import puppeteer from 'puppeteer-core'
+
+export default async function (req, res) {
+    // Edge executable will return an empty string locally.
+    const executablePath = await edgeChromium.executablePath
+    
+    const browser = await puppeteer.launch({
+      executablePath,
+      args: edgeChromium.args,
+      headless: true,
+    })
+    const [page] = await browser.pages();
+
+    await page.goto('https://animixplay.to/v1/do-it-yourself/ep8', { waitUntil: 'networkidle0' });
+
+    const data = await page.content()
+    console.log(data);
+
+    await browser.close();
+    
+    res.send(data)
+  }
